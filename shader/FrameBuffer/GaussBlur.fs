7c3b894c4d1b0f2199465ab7563627656c15e3f0
YuanMu Xu <xuyuanmu@outlook.com>
2023-12-25 18:35:14 +0800

    Use bilinear Gaussian instead of the original Gaussian solver to improve performance

--- shader/FrameBuffer/GaussBlur.fs
+++ shader/FrameBuffer/GaussBlur.fs
@@ -6,27 +6,29 @@ in vec2 TexCoords;
 uniform sampler2D image;
 
 uniform bool horizontal;
-
-uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+uniform float offset[3] = float[](0.0, 1.3846153846, 3.2307692308);
+// uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+uniform float weight[3] = float[](0.2270270270, 0.3162162162, 0.0702702703);
 
 void main()
 {
+    vec2 texSize = textureSize(image, 0); 
     vec2 tex_offset = 1.0 / textureSize(image, 0); // gets size of single texel
     vec3 result = texture(image, TexCoords).rgb * weight[0]; // current fragment's contribution
     if(horizontal)
     {
-        for(int i = 1; i < 5; ++i)
+        for(int i = 1; i < 3; ++i)
         {
-            result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-            result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+            result += texture(image, TexCoords + vec2(offset[i] / texSize.x, 0.0)).rgb * weight[i];
+            result += texture(image, TexCoords - vec2(offset[i] / texSize.x, 0.0)).rgb * weight[i];
         }
     }
     else
     {
-        for(int i = 1; i < 5; ++i)
+        for(int i = 1; i < 3; ++i)
         {
-            result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-            result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+            result += texture(image, TexCoords + vec2(0.0, offset[i] / texSize.y)).rgb * weight[i];
+            result += texture(image, TexCoords - vec2(0.0, offset[i] / texSize.y)).rgb * weight[i];
         }
     }
     FragColor = vec4(result, 1.0);
